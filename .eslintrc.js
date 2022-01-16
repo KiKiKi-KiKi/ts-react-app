f1b5bc8d3fef2b0276e8dd8c128bb677b3e9dd62
KiKiKi <chaika.design@gmail.com>
2022-01-16 18:04:01 +0900

    :star2: Format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,41 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "react-app",
-        "react-app/jest",
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "newline-before-return": "error",
-        "no-console": "warn",
-        "no-var": "error",
-        "no-dupe-class-members": "error",
-        "object-shorthand": ["error", "always"],
-        "prefer-arrow-callback": "error",
-        "prefer-const": "error",
-        "prefer-spread": "error",
-        "require-yield": "error",
-        // don't need import React
-        "react/react-in-jsx-scope": "off"
-    }
-}
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'newline-before-return': 'error',
+    'no-console': 'warn',
+    'no-var': 'error',
+    'no-dupe-class-members': 'error',
+    'object-shorthand': ['error', 'always'],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'prefer-spread': 'error',
+    'require-yield': 'error',
+    // don't need import React
+    'react/react-in-jsx-scope': 'off',
+  },
+};
